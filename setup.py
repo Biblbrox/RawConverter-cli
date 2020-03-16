e588f4f1c8bc2e57033d9c2c7b8ad47dfd02557d
Aleksey Kuchkov <biblbroxxx@gmail.com>
2020-03-17 01:58:44 +0300

    Fix license name in setup.pu

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU GPLv3 License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
